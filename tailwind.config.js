5f1c686d9ebd862b8153ca93b79919ba9c5d85d9
maulanasujarwadi <113002943+maulanasujarwadi@users.noreply.github.com>
2023-12-20 00:05:55 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      colors: {
+        primary: '#54A7B6',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+      fontFamily: {
+        poppins: ['Inter']
+      }
+    },
+  },
+  plugins: [],
+}
